f5fd3e8e7367a9b8e15bdbf7d4efa127dcdc7f86
Craig Smith <craig.smith@768bit.com>
2019-02-26 03:37:44 +0000

    initial commit

--- scripts/build-docker-images-reg.sh
+++ scripts/build-docker-images-reg.sh
@@ -6,3 +6,4 @@ IMAGE_REGISTRY_PATH="registry.gitlab.768bit.com/github/vpkg/docker"
 ##docker build -t $IMAGE_REGISTRY_PATH/vpkg-macos docker/vpkg-macos
 
 docker build -t $IMAGE_REGISTRY_PATH/vpkg-win-msi docker/vpkg-win-msi
+docker push $IMAGE_REGISTRY_PATH/vpkg-win-msi
