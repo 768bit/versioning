fe0b1bbe14d53932eaf4e4acb620dae2b5cb887c
Craig Smith <craig.smith@768bit.com>
2019-12-20 01:13:12 +0000

    Fixes

--- main.go
+++ main.go
@@ -4,3 +4,6 @@ import "github.com/768bit/vpkg/common"
 
 type VersionData = common.VersionData
 type PkgVersionRevisions = common.PkgVersionRevisions
+
+var LoadVersionData = common.LoadVersionData
+var NewVersionData = common.NewVersionData
